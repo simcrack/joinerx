b34fccf691913fd94553b2ffdfe3f4cc45032788
Simon Reichenbach <simon.reichenbach@ad.informaticon.com>
2017-09-10 08:28:26 +0200

    Elementerstelung objektorientiert implementiert Anzeige der ELementdaten im Datapicker implementiert

--- js/main.js
+++ js/main.js
@@ -1,41 +1,17 @@
+//	load addition js files
+document.writeln("<script type='text/javascript' src='js/framework.js'></script>"); //adds class Datapicker
+document.writeln("<script type='text/javascript' src='js/element.js'></script>"); //adds class Element
+document.writeln("<script type='text/javascript' src='js/elements.js'></script>"); //adds class Elements
+document.writeln("<script type='text/javascript' src='js/datapicker.js'></script>"); //adds class Datapicker
+
 function init() {
-	var e = document.getElementsByClassName("element_add");
-	for(var i = 0; i < e.length; i++) {
-		e[i].onclick = function() { addElement(this.id); };
+	var e		= document.getElementsByClassName("element_add");
+	var elen	= e.length;
+	for(var i = 0; i < elen; i++) {
+		e[i].onclick = function() { workbench.pushElement(this.id); };
 	}
-}
-
-/*
-*	add an element to the existing array
-*	
-*	@param position	specifies if the at which position the element shall be added
-*					allowed values are the div.ids "element_left" and "element_right"
-*/
-function addElement(position) {
-	var wbDiv	= document.getElementById("workbench");
-	var newDiv	= document.createElement("div");
-	newDiv.className	= "element";
-	newDiv.id 			= getNextElementId();
+	framework	= new Framework();
+	workbench	= new Elements("workbench");
+	datapicker	= new Datapicker("datapicker");
 	
-	switch(position) {
-		case 'element_left':
-			wbDiv.insertBefore(newDiv, wbDiv.childNodes[2])
-			break;
-		case 'element_right':
-			wbDiv.insertBefore(newDiv, document.getElementById('element_right'));
-			break;
-		default:
-			break;
-	}
-}
-
-
-/*	
-*	function which retrieves next higher, free id in the element-array
-*	
-*	@return	string	next higher id (smth lile "element5")
-*/
-function getNextElementId() {
-	//--//not coded yet
-	return "element5";
 }
